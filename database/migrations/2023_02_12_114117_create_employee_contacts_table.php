d656cda8c4651205a698db742856d77c3fdab3ca
Rubelkhan1997 <eng.rubelkhan797991@gmail.com>
2023-02-12 23:26:38 +0600

    first commit

---
+++ database/migrations/2023_02_12_114117_create_employee_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_contacts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->index();
+            $table->string('contact_name');
+            $table->string('contact_email');
+            $table->string('contact_phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_contacts');
+    }
+}
---
+++ database/migrations/2023_02_12_114220_create_employee_attendances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->index();
+            $table->date('date');
+            $table->timestamp('in_time');
+            $table->timestamp('out_time');
+            $table->timestamp('stay_time');
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_attendances');
+    }
+}
